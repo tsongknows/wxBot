660123c9cf1812a57a3dd4c19d2744d6b3f9155a
Song Tiansheng <tiansheng@SongdeMacBook-Pro.local>
2016-09-18 00:36:36 +0800

    refactor for some repeat logic

--- bot.py
+++ bot.py
@@ -63,36 +63,50 @@ class TulingWXBot(WXBot):
     def handle_msg_all(self, msg):
         if not self.robot_switch and msg['msg_type_id'] != 1:
             return
-        if msg['msg_type_id'] == 1 and msg['content']['type'] == 0:  # reply to self
+
+        msgType0 = msg['content']['type'] == 0
+        if not msgType0:
+            return
+
+        if msg['msg_type_id'] == 1:  # reply to self
             self.auto_switch(msg)
-        elif msg['msg_type_id'] == 4 and msg['content']['type'] == 0:  # text message from contact
-            self.send_msg_by_uid(self.tuling_auto_reply(msg['user']['id'], msg['content']['data']), msg['user']['id'])
-        elif msg['msg_type_id'] == 3 and msg['content']['type'] == 0:  # group text message
-            if 'detail' in msg['content']:
-                my_names = self.get_group_member_name(self.my_account['UserName'], msg['user']['id'])
-                if my_names is None:
-                    my_names = {}
-                if 'NickName' in self.my_account and self.my_account['NickName']:
-                    my_names['nickname2'] = self.my_account['NickName']
-                if 'RemarkName' in self.my_account and self.my_account['RemarkName']:
-                    my_names['remark_name2'] = self.my_account['RemarkName']
+        elif msg['msg_type_id'] == 4:  # text message from contact
+            reply = self.tuling_auto_reply(msg['user']['id'], msg['content']['data']);
+            self.send_msg_by_uid(reply, msg['user']['id'])
+        elif msg['msg_type_id'] == 3:  # group text message
+            if not 'detail' in msg['content']:
+                return
 
-                is_at_me = False
-                for detail in msg['content']['detail']:
-                    if detail['type'] == 'at':
-                        for k in my_names:
-                            if my_names[k] and my_names[k] == detail['value']:
-                                is_at_me = True
-                                break
-                if is_at_me:
-                    src_name = msg['content']['user']['name']
-                    reply = 'to ' + src_name + ': '
-                    if msg['content']['type'] == 0:  # text message
-                        reply += self.tuling_auto_reply(msg['content']['user']['id'], msg['content']['desc'])
-                    else:
-                        reply += u"对不起，只认字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
-                    self.send_msg_by_uid(reply, msg['user']['id'])
+            is_at_me = isAtMe(msg, getMyNames())
+            if not is_at_me:
+                return
 
+            src_name = msg['content']['user']['name']
+            reply = 'to ' + src_name + ': '
+            if msg['content']['type'] == 0:  # text message
+                reply += self.tuling_auto_reply(msg['content']['user']['id'], msg['content']['desc'])
+            else:
+                reply += u"对不起，只认字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
+            self.send_msg_by_uid(reply, msg['user']['id'])
+
+def getMyNames():
+    my_names = self.get_group_member_name(self.my_account['UserName'], msg['user']['id'])
+    if my_names is None:
+        my_names = {}
+    if 'NickName' in self.my_account and self.my_account['NickName']:
+        my_names['nickname2'] = self.my_account['NickName']
+    if 'RemarkName' in self.my_account and self.my_account['RemarkName']:
+        my_names['remark_name2'] = self.my_account['RemarkName']
+    
+    return my_names
+
+def isAtMe(msg,my_names):
+    for detail in msg['content']['detail']:
+        if detail['type'] == 'at':
+            for k in my_names:
+                if my_names[k] and my_names[k] == detail['value']:
+                    return True
+    return False
 
 def main():
     bot = TulingWXBot()
--- test.py
+++ test.py
@@ -6,10 +6,23 @@ from wxbot import *
 
 class MyWXBot(WXBot):
     def handle_msg_all(self, msg):
+        print str(msg)
+        content = msg['content']['data']
         if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
             self.send_msg_by_uid(u'hi', msg['user']['id'])
             #self.send_img_msg_by_uid("img/1.png", msg['user']['id'])
             #self.send_file_msg_by_uid("img/1.png", msg['user']['id'])
+        elif msg['msg_type_id'] == 1 and msg['content']['type'] == 0:  # reply to self
+            if content == u'开启':
+                self.send_msg_by_uid(u'[Robot]' + u'机器人已开启！', msg['to_user_id'])
+        elif msg['msg_type_id'] == 4 and msg['content']['type'] == 0:  # text message from contact
+            self.send_msg_by_uid(msg['content']['data'], msg['user']['id'])
+        elif msg['msg_type_id'] == 3:  # group text message
+            if  msg['content']['type'] == 0:
+                reply = content
+            else:
+                reply = u"对不起，只认字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
+            self.send_msg_by_uid(reply, msg['user']['id'])
 '''
     def schedule(self):
         self.send_msg(u'张三', u'测试')
